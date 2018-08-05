d5331974e26392cec4a98decaff5726428fdbbd8
Andrey Volkov <AndreyVolkovHSEBI@yandex.ru>
2018-08-05 18:09:21 +0300

    registration page design

---
+++ app/src/main/java/style/ru/schoolapp/RegistrationActivity.java
@@ -0,0 +1,13 @@
+package style.ru.schoolapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
