426fb1dcb2cc3df0dc50a6abe1dbc64e1ff18856
Andrey Volkov <AndreyVolkovHSEBI@yandex.ru>
2018-08-06 19:31:13 +0300

    navigation drawer design

--- app/src/main/java/style/ru/schoolapp/MyAccountActivity.java
+++ app/src/main/java/style/ru/schoolapp/MyAccountActivity.java
@@ -4,14 +4,20 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import butterknife.BindView;
 
 public class MyAccountActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
 
     //@BindView(R.id.userImage) ImageView userImage;
 
@@ -24,6 +30,18 @@ public class MyAccountActivity extends AppCompatActivity {
         toolbar.setTitle("Личный кабинет");
         setSupportActionBar(toolbar);
 
+        drawerLayout = findViewById(R.id.drawer);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
