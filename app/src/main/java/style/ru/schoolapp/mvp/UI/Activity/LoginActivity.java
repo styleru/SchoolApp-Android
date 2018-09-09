cec7e39b8aeed363573372be5b8c73cbbe0ee7e8
eldarovich99 <32201422+eldarovich99@users.noreply.github.com>
2018-09-09 16:23:11 +0300

    initial commit

--- app/src/main/java/style/ru/schoolapp/mvp/UI/Activity/LoginActivity.java
+++ app/src/main/java/style/ru/schoolapp/mvp/UI/Activity/LoginActivity.java
@@ -21,28 +21,5 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void init() {
-        Button button_enter = (Button) findViewById(R.id.Enter);
-        button_enter.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("ResourceAsColor")
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), EnrollmentActivity.class);
-                EditText login = (EditText) findViewById(R.id.Login);
-                EditText password = (EditText) findViewById(R.id.Password);
-                String lg_string = login.getText().toString();
-                String pw_string = password.getText().toString();
-                if (lg_string.equals("login") && pw_string.equals("password")) {
-                    startActivity(intent);
-                }
-                else {
-                    Toast.makeText(LoginActivity.this, "Такого пользователя не существует", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-    }
-
-    public void signUpClick(View view) {
-        Intent intent = new Intent(LoginActivity.this, RegistrationActivity.class);
-        startActivity(intent);
     }
 }
